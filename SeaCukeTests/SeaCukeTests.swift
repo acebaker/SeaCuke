9e5ae2aa3b7c10528d2d693c6886fe069a0886a0
acebaker <jeremy.juel@gmail.com>
2015-11-11 16:39:34 -0800

    Initial Commit

---
+++ SeaCukeTests/SeaCukeTests.swift
@@ -0,0 +1,36 @@
+//
+//  SeaCukeTests.swift
+//  SeaCukeTests
+//
+//  Created by Jeremy Juel on 11/11/15.
+//  Copyright © 2015 Jeremy Juel. All rights reserved.
+//
+
+import XCTest
+@testable import SeaCuke
+
+class SeaCukeTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
